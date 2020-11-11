7ac572ef7d203635fa5ef388988b3787ac37e1b3
Robbe Van Looy <robbe.vanlooy@student.hogent.be>
2020-11-11 22:27:46 +0100

    custom splash screen

---
+++ TraveloreFE/View/ExtendedSplash.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TraveloreFE.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    partial class ExtendedSplash : Page
+    {
+        internal Rect splashImageRect;
+        private SplashScreen splash;
+        internal bool dismissed = false;
+        internal Frame rootFrame;
+
+        public ExtendedSplash(SplashScreen splashScreen, bool loadState)
+        {
+            InitializeComponent();
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+            splash = splashScreen;
+            if(splash!= null)
+            {
+                splash.Dismissed += new TypedEventHandler<SplashScreen, object>(DismissedEventHandler);
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+                PositionRing();
+            }
+            rootFrame = new Frame();
+        }
+
+        void PositionImage()
+        {
+            extendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            extendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            extendedSplashImage.Height = splashImageRect.Height;
+            extendedSplashImage.Width = splashImageRect.Width;
+        }
+
+        void PositionRing()
+        {
+            splashProgressRing.SetValue(Canvas.LeftProperty, splashImageRect.X + (splashImageRect.Width * 0.5) - (splashProgressRing.Width * 0.5));
+            splashProgressRing.SetValue(Canvas.TopProperty, (splashImageRect.Y + splashImageRect.Height + splashImageRect.Height * 0.1));
+        }
+
+        void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+        }
+
+        async void DismissExtendedSplash()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                rootFrame = new Frame();
+                rootFrame.Content = new MainPage(); Window.Current.Content = rootFrame;
+            });
+        }
+
+        void ExtendedSplash_OnResize(Object sender, WindowSizeChangedEventArgs e)
+        {
+            // Safely update the extended splash screen image coordinates. This function will be executed when a user resizes the window.
+            if (splash != null)
+            {
+                // Update the coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+                PositionRing();
+            }
+        }
+
+        void DismissSplashButton_Click(object sender, RoutedEventArgs e)
+        {
+            DismissExtendedSplash();
+        }
+    }
+}
